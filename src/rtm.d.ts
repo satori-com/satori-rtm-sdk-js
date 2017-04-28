a8e2355a3235553ebd1f1db39ea24a15635ae425
Andrey Vasenin <avasenin@satori.com>
2017-04-28 13:02:26 +0200

    Fix typescript definition

--- src/rtm.d.ts
+++ src/rtm.d.ts
@@ -1,9 +1,10 @@
 declare module 'satori-sdk-js' {
   export = RTM;
 
-  type OnAck = (msg: any) => void;
-  type OnEvent = (msg: any) => void;
-  type OnCompleted = (msg: any) => void;
+  type OnAck = (pdu: PDU) => void;
+  type OnEvent = (...args: any[]) => void;
+  type OnCompleted = (subscription: RTM.Subscription) => void;
+  type AuthProvider = (rtm: RTM, onsuccess: () => void, onerror: (err: any) => void) => void;
 
   interface Observer {
     on(event: string, handler: OnEvent): void;
@@ -11,20 +12,46 @@ declare module 'satori-sdk-js' {
     fire(event: string): void;
   }
 
+  interface PDU {
+    id: number;
+    body: any;
+    position: string;
+  }
+
+  interface AuthProviderOptions {
+    timeout?: number;
+  }
+
+  interface RTMOptions {
+    minReconnectInterval?: number;
+    maxReconnectInterval?: number;
+    authProvider?: AuthProvider;
+    heartbeatEnabled?: boolean;
+    heartbeatInterval?: number;
+    highWaterMark?: number;
+    lowWaterMark?: number;
+    checkWritabilityInterval?: number;
+  }
+
+  interface ReadOptions {
+    bodyOpts?: any;
+    onAck?: OnAck;
+  }
+
   class RTM implements Observer {
-    constructor(endpoint: string, appkey: string, options?: any);
+    constructor(endpoint: string, appkey: string, options?: RTMOptions);
     start(): void;
     stop(): void;
     restart(): void;
     isStopped(): boolean;
     isConnected(): boolean;
     getSubscription(subId: string): RTM.Subscription;
-    subscribe(subId: string, mode: RTM.SubscriptionMode, options?: any): RTM.Subscription;
-    resubscribe(subId: string, options?: any, onCompleted?: OnCompleted): void;
+    subscribe(subId: string, mode: RTM.SubscriptionMode, bodyOpts?: any): RTM.Subscription;
+    resubscribe(subId: string, mode: RTM.SubscriptionMode, bodyOpts?: any, onCompleted?: OnCompleted): void;
     unsubscribe(subId: string, onAck?: OnAck): void;
     publish(channel: string, message: any, onAck?: OnAck): void;
     read(channel: string, onAck?: OnAck): void;
-    read(channel: string, options?: any): void;
+    read(channel: string, options?: ReadOptions): void;
     write(channel: string, value: any, onAck?: OnAck): void;
     delete(channel: string, onAck?: OnAck): void;
     search(channel: string, onAck?: OnAck): void;
@@ -35,9 +62,11 @@ declare module 'satori-sdk-js' {
   }
 
   module RTM {
-    interface Subscription extends Observer { }
+    interface Subscription extends Observer {
+      subscriptionId: string;
+    }
 
-    function roleSecretAuthProvider(role: string, roleSecret: string, opts: any, timeout?: number): any;
+    function roleSecretAuthProvider(role: string, roleSecret: string, opts?: AuthProviderOptions): AuthProvider;
 
     interface SubscriptionMode {
       trackPosition: boolean;
@@ -51,4 +80,3 @@ declare module 'satori-sdk-js' {
     }
   }
 }
-
